7c4cd9b74d448b6d32330bcb89c91921ad57ad23
maratheking <79169003+maratheking@users.noreply.github.com>
2021-02-16 21:29:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+let x = 0;
+
+var para = document.createElement("button");  
+var node = document.createTextNode("Wanna enter the rabbit hole? " );
+para.appendChild(node);
+
+var element = document.getElementById("div1");
+ 
+
+
+
+function drogiranje( ) {
+     x++;
+    element.appendChild(para);
+}
+
+alert('aaa')
+
+
